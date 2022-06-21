ee008e01d07c0127718c874cfb5b703d42fa3707
Valéria <valeriamenezes022@gmail.com>
2022-06-21 16:26:30 -0300

    criando arquivo scripts.js

---
+++ 01-fundamentos/bloco-04-introdução-javascript-e-lógica-de-programação/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Valéria";
+const birthCity = "Senador Canedo";
+let birthYear = 2004;
+
+birthYear= 2030;
+birthCity= "Jataí";
+
+console.log (myName);
+console.log (birthYear);
+console.log (birthCity);
